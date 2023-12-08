d25982b18812e3db3e2e525c223841a6b5b2ee13
Ryan Scott <ryanamscott52@gmail.com>
2023-12-08 16:23:13 -0600

    Adds collapsible navbar to pages. Cleans up HTML and CSS files for a more simplistic layout.

---
+++ script.js
@@ -0,0 +1,7 @@
+// This code is for the collapsible navbar
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
