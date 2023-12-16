7093a029dd611b5f686f56c811292ae967bfa22c
Ryan Scott <ryanamscott52@gmail.com>
2023-12-16 11:16:33 -0600

    Commits final changes before switching to a SPA site.

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+<header class="header">
+<nav class="nav-bar">
+  <Div><a href="index.html" class="brand-title">SimpleConversions</a></Div>
+  <a href="#" class="toggle-button">
+    <span class="bar"></span>
+    <span class="bar"></span>
+    <span class="bar"></span>
+  </a>
+  <div class="navbar-links">
+    <ul>
+   <li>
+    <a href="about.html" class="nav-link">About Us</a>
+   </li>
+   <li>
+    <a href="contact.html" class="nav-link">Contact Us</a>
+   </li>
+   <li>
+    <a href="imperialsystem.html" class="nav-link">US/Imperial System</a>
+   </li>
+   <li>
+    <a href="metricsystem.html" class="nav-link">Metric System</a>
+   </li>
+   <li>
+    <a href="learningtools.html" class="nav-link">Learning Tools</a>
+   </li>
+  </ul>
+</div>
+</nav>
+</header>
